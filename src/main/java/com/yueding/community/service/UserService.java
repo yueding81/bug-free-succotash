e746a29ab384ad54912d21010b5e29c11fee8b13
yueding <ljiahao382@gmail.com>
2023-06-12 18:52:21 +0800

    第二次提交

--- src/main/java/com/yueding/community/service/UserService.java
+++ src/main/java/com/yueding/community/service/UserService.java
@@ -2,15 +2,32 @@ package com.yueding.community.service;
 
 import com.yueding.community.dao.UserMapper;
 import com.yueding.community.entity.User;
+import com.yueding.community.util.MailClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
 
 @Service
 public class UserService {
     @Autowired
     private UserMapper userMapper;
 
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Value("${community.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
     public User selectById(int id){
         return userMapper.selectById(id);
     }
+
+
 }
